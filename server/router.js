92fadd3163316d52e4d23ce668547a21ab9414fe
Alex Kott <tetlokax@gmail.com>
2017-02-15 11:23:34 +0100

    adds unshift to Queue

--- server/router.js
+++ server/router.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const Queue = require('promise-queue');
+const Queue = require('./Queue.js');
 const fileManager = require('./fileManager');
 const converter = require('./converter');
 const NotificationService = require('./notificationService');
 const notificationService = new NotificationService();
 
-const htmlQueue = new Queue(1, Infinity);
-const pdfQueue = new Queue(2, Infinity);
+const queue = new Queue(1, Infinity);
 
 router.route('/api')
     .get((req, res) => {
@@ -18,33 +17,24 @@ router.route('/api')
     })
     .post((req ,res) => {
         const type = req.body.type;
+        let queueingType = 'add';
         fileManager.create({ name: req.body.name, type, createdAt: new Date() });
 
         if (type === 'html') {
-            htmlQueue
-                .add(() => {
-                    return converter.save(type, req.body.name, req.body.content);
-                })
-                .then((info) => {
-                    notificationService.broadcast('update', `${info.fileName} was converted to ${info.fileType} and is ready to download.`);
-                })
-                .catch((error) => {
-                    notificationService.broadcast('error', 'There was an error converting the file.');
-                });
-        } else if (type === 'pdf') {
-            pdfQueue
-                .add(() => {
-                    return converter.save(type, req.body.name, req.body.content);
-                })
-                .then((info) => {
-                    notificationService.broadcast('update', `${info.fileName} was converted to ${info.fileType} and is ready to download.`);
-                })
-                .catch((error) => {
-                    notificationService.broadcast('error', 'There was an error converting the file.');
-                });
+            queueingType = 'unshift';
         }
-        const queueLength = htmlQueue.getQueueLength() + pdfQueue.getQueueLength();
-        const pendingLength = htmlQueue.getPendingLength() + pdfQueue.getPendingLength();
+        queue[queueingType](() => {
+                return converter.save(type, req.body.name, req.body.content);
+            })
+            .then((info) => {
+                notificationService.broadcast('update', `${info.fileName} was converted to ${info.fileType} and is ready to download.`);
+            })
+            .catch((error) => {
+                console.log(error);
+                notificationService.broadcast('error', 'There was an error converting the file.');
+            });
+        const queueLength = queue.getQueueLength();
+        const pendingLength = queue.getPendingLength();
 
         res.send({
             message: `File is being converted. Currently converting ${pendingLength} files, ${queueLength} files on the waiting list.`
